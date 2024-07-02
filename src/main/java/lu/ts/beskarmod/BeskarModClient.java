c9d4b9a1a5e7431ee7c759b75d30e714f452c513
Thibault Savenkoff <thibault.savenkoff@outlook.fr>
2024-07-02 18:15:51 +0200

    Init Commit

---
+++ src/main/java/lu/ts/beskarmod/BeskarModClient.java
@@ -0,0 +1,10 @@
+package lu.ts.beskarmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeskarModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/lu/ts/beskarmod/BeskarModDataGenerator.java
@@ -0,0 +1,11 @@
+package lu.ts.beskarmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BeskarModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
